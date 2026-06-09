ee42f2159e66d356c773dacddd35879692c78b0f
Souad <souadkhalifeh620@gmail.com>
2026-06-09 15:02:35 +0300

    Tuples exercises

---
+++ collections_exercises/tuples_exercises.py
@@ -0,0 +1,15 @@
+#tuples: orders, unchangeable, allows duplicate values 
+
+my_tuple = (1, 2, 3, 4, 5)
+print(len(my_tuple))
+
+print(type(my_tuple))
+
+tuple1= ('souad', 123, True)
+print(type(tuple1))
+
+print(tuple1[1])
+
+print(tuple1[-1])
+
+print(my_tuple[2:5])
