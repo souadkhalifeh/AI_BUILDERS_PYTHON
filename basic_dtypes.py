9efcb97cd087f7fb2790c5a464990259eee8cf63
Souad <souadkhalifeh620@gmail.com>
2026-06-09 14:19:07 +0300

    Basic dtypes of python

---
+++ basic_dtypes.py
@@ -0,0 +1,19 @@
+print("hello")
+x =1 #integer 
+y = 1.5 #float
+z = "hello" #string
+v = 'hello'
+
+t = """
+this is a multi line string
+it can span multiple lines
+"""
+
+u= True #boolean
+w = False
+
+print(type(w))
+
+#Casting 
+a = int(1.5) #casting float to int
+print (a)
